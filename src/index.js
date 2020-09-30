9906b5b67db8b8cafab50c3ffb75575681a6e37a
sametweb <sametmutevellioglu@gmail.com>
2020-09-30 04:51:34 -0700

    serviceWorker is activated

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "antd/dist/antd.css";
 import "./style.css";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+
+serviceWorker.register();
