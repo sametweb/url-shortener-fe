3088fc8824f6cc41579887cb7b903303a2ceeabb
sametweb <sametmutevellioglu@gmail.com>
2020-09-30 04:30:28 -0700

    input label for w3

--- src/components/Input.js
+++ src/components/Input.js
@@ -5,11 +5,15 @@ import Search from "antd/lib/input/Search";
 function Input({ shortenUrl, validError }) {
   return (
     <>
+      <label htmlFor="url-input" style={{ display: "none" }}>
+        URL Input
+      </label>
       <Search
         placeholder="Input long URL"
         enterButton="Shorten"
         size="large"
         onSearch={shortenUrl}
+        id="url-input"
       />
       {validError && <span className="error">{validError}</span>}
     </>
