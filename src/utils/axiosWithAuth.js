f5473ad0bf0431c71fd9fd1e763fcac5a4c8bf59
sametweb <sametmutevellioglu@gmail.com>
2020-10-05 02:17:37 -0700

    headers auth

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -4,6 +4,7 @@ export default (token) => {
   const params = {
     baseURL: process.env.REACT_APP_BACK_END,
     headers: { "Content-Type": "application/json" },
+    withCredentials: true,
   };
   if (token) {
     params.headers = { authorization: token };
