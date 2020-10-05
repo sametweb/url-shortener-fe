54d08411101e74f2a4aeebac43982742d1e3a855
sametweb <sametmutevellioglu@gmail.com>
2020-10-05 03:21:46 -0700

    headers auth

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 export default (token) => {
-  const params = {
-    baseURL: process.env.REACT_APP_BACK_END,
-    withCredentials: true,
-  };
+  const params = { baseURL: process.env.REACT_APP_BACK_END };
   if (token) {
     params.headers = { authorization: token };
   }
