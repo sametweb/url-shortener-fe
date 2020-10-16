553c411bbaa7c374e064ee726998947aee4331dc
sametweb <sametmutevellioglu@gmail.com>
2020-10-16 05:33:35 -0700

    google webmaster tools verification

--- src/utils/validateUrl.js
+++ src/utils/validateUrl.js
@@ -1,4 +1,4 @@
 export default function validateUrl(url) {
-  const regex = /^(?:http(s)?:\/\/)?[\w.-]+(?:.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/gm;
+  const regex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/g;
   return regex.test(url);
 }
